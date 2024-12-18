de277f2b3effad10b8d08a6f3a99907fe1462280
maxb-io <105273783+maxb-io@users.noreply.github.com>
2024-12-18 19:14:21 +0200

    Create install_cmake.sh

---
+++ install/install_cmake.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+version=3.25.1
+processor=$(uname -m)
+OS_TYPE=$(uname -s)
+OS_NAME=$(grep '^NAME=' /etc/os-release | sed 's/"//g')
+OS_NAME=${OS_NAME#"NAME="}
+MODE=$1 # whether to install using sudo or not
+
+if [[ $OS_TYPE = 'Darwin' ]]
+then
+    brew install cmake
+else
+    if [[ $OS_NAME == 'Alpine Linux' ]]
+    then
+        $MODE apk add --no-cache cmake
+    else
+        if [[ $processor = 'x86_64' ]]
+        then
+            filename=cmake-${version}-linux-x86_64.sh
+        else
+            filename=cmake-${version}-linux-aarch64.sh
+        fi
+
+        wget https://github.com/Kitware/CMake/releases/download/v${version}/${filename}
+        chmod u+x ./${filename}
+        $MODE ./${filename} --skip-license --prefix=/usr/local --exclude-subdir
+        cmake --version
+    fi
+fi
